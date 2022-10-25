326f92a9f96c6643df6d69d82569dc81feb70f4c
RichardM0 <86446417+RichardM0@users.noreply.github.com>
2022-10-25 08:21:44 -0400

    stuff

--- searchbar.js
+++ searchbar.js
@@ -119,7 +119,7 @@ mit6001x.name = "mit6001x"; //what will the search query be?
 mit6001x.image = "ics3up-MITx6001x.jpeg"; //link to image for the course (relative to results.html)
 mit6001x.display = "MIT's Introduction to Computer Science with Python"; //what will the bolded heading say?
 mit6001x.desc = "Intermediate and beginner course for those interested in programming with Python! Includes complexity, data types, and more."; //what will the description say?
-mit6001x.link = "../ics3up-lrnX-final/courseslist/mit6001x.html"; //what should it be linked to? (relative to results.html)
+mit6001x.link = "courseslist/mit6001x.html"; //what should it be linked to? (relative to results.html)
 mit6001x.keywords = ["mit","python","6001x","introduction"]; //some keywords that will also bring up the course
 pages.push(mit6001x);
 
@@ -129,7 +129,7 @@ CS50W.name = "CS50W"; //what will the search query be?
 CS50W.image = "ics3up-CS50W.webp"; //link to image for the course (relative to results.html)
 CS50W.display = "CS50W's Web Development with Python and JavaScript"; //what will the bolded heading say?
 CS50W.desc =  "Learn Web Development using Python, Javascript, Django, SQL and more!"; //what will the description say?
-CS50W.link = "../ics3up-lrnX-final/courseslist/CS50W.html"; //what should it be linked to? (relative to results.html)
+CS50W.link = "courseslist/CS50W.html"; //what should it be linked to? (relative to results.html)
 CS50W.keywords = ["javascript","python","harvard","cs50", "web development", "web programming"]; //some keywords that will also bring up the course
 pages.push(CS50W);
 
@@ -138,7 +138,7 @@ CS50P.name = "CS50P"; //what will the search query be?
 CS50P.image = "ics3up-CS50P.jpeg"; //link to image for the course (relative to results.html)
 CS50P.display = "CS50P's Introduction to Computer Science with Python"; //what will the bolded heading say?
 CS50P.desc =  "Learn how to program using scripting language Python!"; //what will the description say?
-CS50P.link = "../ics3up-lrnX-final/courseslist/CS50P.html"; //what should it be linked to? (relative to results.html)
+CS50P.link = "courseslist/CS50P.html"; //what should it be linked to? (relative to results.html)
 CS50P.keywords = ["python","harvard","cs50","introduction","beginning"]; //some keywords that will also bring up the course
 pages.push(CS50P);
 
@@ -147,7 +147,7 @@ CS50x.name = "CS50x"; //what will the search query be?
 CS50x.image = "ics3up-CS50X.png"; //link to image for the course (relative to results.html)
 CS50x.display = "CS50x's Introduction to Computer Science"; //what will the bolded heading say?
 CS50x.desc =  "Use C++ to learn how to program, along with Computer Science fundamentals such as binary, how a computer works, experiments, and more."; //what will the description say?
-CS50x.link = "../ics3up-lrnX-final/courseslist/CS50x.html"; //what should it be linked to? (relative to results.html)
+CS50x.link = "courseslist/CS50x.html"; //what should it be linked to? (relative to results.html)
 CS50x.keywords = ["C++","harvard","cs50","introduction","beginning",]; //some keywords that will also bring up the course
 pages.push(CS50x);
 
@@ -156,7 +156,7 @@ CS50M.name = "CS50M"; //what will the search query be?
 CS50M.image = "ics3up-CS50M.webp"; //link to image for the course (relative to results.html)
 CS50M.display = "CS50M: Mobile App Development with React Native"; //what will the bolded heading say?
 CS50M.desc =  " Learn about mobile app development with React Native, a popular framework maintained by Facebook that enables cross-platform native apps using JavaScript without Java or Swift."; //what will the description say?
-CS50M.link = "../ics3up-lrnX-final/courseslist/CS50M.html"; //what should it be linked to? (relative to results.html)
+CS50M.link = "courseslist/CS50M.html"; //what should it be linked to? (relative to results.html)
 CS50M.keywords = ["react native","react","harvard","cs50","web programming","javascript", "web development",]; //some keywords that will also bring up the course
 pages.push(CS50M);
 
@@ -165,7 +165,7 @@ CS50AI.name = "CS50AI"; //what will the search query be?
 CS50AI.image = "ics3up-CS50AI.webp"; //link to image for the course (relative to results.html)
 CS50AI.display = "CS50AI: Introduction to Artificial Intelligence with Python"; //what will the bolded heading say?
 CS50AI.desc =  "Learn to use machine learning in Python in this introductory course on artificial intelligence."; //what will the description say?
-CS50AI.link = "../ics3up-lrnX-final/courseslist/CS50AI.html"; //what should it be linked to? (relative to results.html)
+CS50AI.link = "courseslist/CS50AI.html"; //what should it be linked to? (relative to results.html)
 CS50AI.keywords = ["ai","artificial intelligence","harvard","cs50","python","introduction", "beginner",]; //some keywords that will also bring up the course
 pages.push(CS50AI);
 
@@ -174,7 +174,7 @@ IBMDSC.name = "IBMDSC++"; //what will the search query be?
 IBMDSC.image = "ics3up-IBMDSC++.webp"; //link to image for the course (relative to results.html)
 IBMDSC.display = "IBM: Data Structures and Algorithms using C++"; //what will the bolded heading say?
 IBMDSC.desc =  "Build efficient programs by learning how to implement data structures using algorithmic techniques and solve various computational problems using the C++ programming language."; //what will the description say?
-IBMDSC.link = "../ics3up-lrnX-final/courseslist/IBMDSC++.html"; //what should it be linked to? (relative to results.html)
+IBMDSC.link = "courseslist/IBMDSC++.html"; //what should it be linked to? (relative to results.html)
 IBMDSC.keywords = ["data science","ibm","algorithm","data structures","data", "c++",]; //some keywords that will also bring up the course
 pages.push(IBMDSC);
 
@@ -183,7 +183,7 @@ IBMFC.name = "IBMFC++"; //what will the search query be?
 IBMFC.image = "ics3up-IBMFC++.webp"; //link to image for the course (relative to results.html)
 IBMFC.display = "IBM: Fundamentals with C++"; //what will the bolded heading say?
 IBMFC.desc =  "An introductory course with C++ that inspires kinesthetic learners!"; //what will the description say?
-IBMFC.link = "../ics3up-lrnX-final/courseslist/IBMFC++.html"; //what should it be linked to? (relative to results.html)
+IBMFC.link = "courseslist/IBMFC++.html"; //what should it be linked to? (relative to results.html)
 IBMFC.keywords = ["starter","ibm","beginner","introduction","fundamentals", "c++",]; //some keywords that will also bring up the course
 pages.push(IBMFC);
 
@@ -192,7 +192,7 @@ IBMWJS.name = "IBMWJS"; //what will the search query be?
 IBMWJS.image = "ics3up-IBMJS.webp"; //link to image for the course (relative to results.html)
 IBMWJS.display = "IBM: Web Developing in the Cloud with JavaScript"; //what will the bolded heading say?
 IBMWJS.desc =  "Using JavaScript and more to learn web development, and web development with the cloud."; //what will the description say?
-IBMWJS.link = "../ics3up-lrnX-final/courseslist/IBMWJS.html"; //what should it be linked to? (relative to results.html)
+IBMWJS.link = "courseslist/IBMWJS.html"; //what should it be linked to? (relative to results.html)
 IBMWJS.keywords = ["cloud","ibm","javascript","web development","front-end", "develop",]; //some keywords that will also bring up the course
 pages.push(IBMWJS);
 
@@ -201,7 +201,7 @@ IBMPDS.name = "IBMPDS"; //what will the search query be?
 IBMPDS.image = "ics3up-IBMPDS.webp"; //link to image for the course (relative to results.html)
 IBMPDS.display = "IBM: Python Basics for Data Science"; //what will the bolded heading say?
 IBMPDS.desc =  "This Python course provides a beginner-friendly introduction to Python for Data Science. Practice through lab exercises, and you'll be ready to create your first Python scripts on your own."; //what will the description say?
-IBMPDS.link = "../ics3up-lrnX-final/courseslist/IBMPDS.html"; //what should it be linked to? (relative to results.html)
+IBMPDS.link = "courseslist/IBMPDS.html"; //what should it be linked to? (relative to results.html)
 IBMPDS.keywords = ["python","ibm","basics","data science","data", "beginner","introduction",]; //some keywords that will also bring up the course
 pages.push(IBMPDS);
 
@@ -210,7 +210,7 @@ IBMLP.name = "IBMLP"; //what will the search query be?
 IBMLP.image = "ics3up-IBMLP.webp"; //link to image for the course (relative to results.html)
 IBMLP.display = "IBM: Machine Learning with Python"; //what will the bolded heading say?
 IBMLP.desc =  "Machine Learning can be an incredibly beneficial tool to uncover hidden insights and predict future trends. This Machine Learning with Python course will give you all the tools you need to get started with supervised and unsupervised learning."; //what will the description say?
-IBMLP.link = "../ics3up-lrnX-final/courseslist/IBMLP.html"; //what should it be linked to? (relative to results.html)
+IBMLP.link = "courseslist/IBMLP.html"; //what should it be linked to? (relative to results.html)
 IBMLP.keywords = ["python","ibm","machine learning","ai","data", "neural","artificial intelligence",]; //some keywords that will also bring up the course
 pages.push(IBMLP);
 
@@ -219,7 +219,7 @@ UoPjs.name = "UoPjs"; //what will the search query be?
 UoPjs.image = "ics3up-UoPjs.webp"; //link to image for the course (relative to results.html)
 UoPjs.display = "UoP: Programming for the Web with JavaScript"; //what will the bolded heading say?
 UoPjs.desc =  "Learn how to develop dynamic, interactive, and data-driven web apps using JavaScript."; //what will the description say?
-UoPjs.link = "../ics3up-lrnX-final/courseslist/UoPjs.html"; //what should it be linked to? (relative to results.html)
+UoPjs.link = "courseslist/UoPjs.html"; //what should it be linked to? (relative to results.html)
 UoPjs.keywords = ["javascript","uop","javascript","web development","front-end", "develop","university of pensylvannia"]; //some keywords that will also bring up the course
 pages.push(UoPjs);
 
@@ -228,7 +228,7 @@ klwebsec.name = "klwebsec"; //what will the search query be?
 klwebsec.image = "ics3up-klwebsec.jpg"; //link to image for the course (relative to results.html)
 klwebsec.display = "Ku Leuven's Web Security Fundamentals"; //what will the bolded heading say?
 klwebsec.desc =  "Essential knowledge for every web developer, discover important principles of modern web security, and learn about current security best practices."; //what will the description say?
-klwebsec.link = "../ics3up-lrnX-final/courseslist/klwebsec.html"; //what should it be linked to? (relative to results.html)
+klwebsec.link = "courseslist/klwebsec.html"; //what should it be linked to? (relative to results.html)
 klwebsec.keywords = ["web security","ku leuven","javascript","web development","front-end", "develop","web"]; //some keywords that will also bring up the course
 pages.push(klwebsec);
 
@@ -237,7 +237,7 @@ W3CHC.name = "W3CHC"; //what will the search query be?
 W3CHC.image = "ics3up-W3CHC.webp"; //link to image for the course (relative to results.html)
 W3CHC.display = "W3C: HTML5 and CSS Fundamentals"; //what will the bolded heading say?
 W3CHC.desc =  "Learn how to build Web sites using HTML5 and basic CSS, directly from W3C, creator of the latest Web standards."; //what will the description say?
-W3CHC.link = "../ics3up-lrnX-final/courseslist/W3CHC.html"; //what should it be linked to? (relative to results.html)
+W3CHC.link = "courseslist/W3CHC.html"; //what should it be linked to? (relative to results.html)
 W3CHC.keywords = ["css","html","javascript","web development","front-end", "develop","web","w3c"]; //some keywords that will also bring up the course
 pages.push(W3CHC);
 
@@ -246,7 +246,7 @@ W3CH.name = "W3CH"; //what will the search query be?
 W3CH.image = "ics3up-W3CH.webp"; //link to image for the course (relative to results.html)
 W3CH.display = "W3C: HTML5 Coding Essentials and Best Practices"; //what will the bolded heading say?
 W3CH.desc =  "Learn how to write Web pages and Web sites by mastering HTML5 coding techniques and best practices."; //what will the description say?
-W3CH.link = "../ics3up-lrnX-final/courseslist/W3CH.html"; //what should it be linked to? (relative to results.html)
+W3CH.link = "courseslist/W3CH.html"; //what should it be linked to? (relative to results.html)
 W3CH.keywords = ["css","fundamentals","basic","beginner","introduction","html","javascript","web development","front-end", "develop","web","w3c"]; //some keywords that will also bring up the course
 pages.push(W3CH);
 
@@ -255,7 +255,7 @@ CS50G.name = "CS50G"; //what will the search query be?
 CS50G.image = "ics3up-CS50G.webp"; //link to image for the course (relative to results.html)
 CS50G.display = "CS50G's Introduction to Game Development"; //what will the bolded heading say?
 CS50G.desc =  "Learn about the development of 2D and 3D interactive games in this hands-on course, as you explore the design of games such as Super Mario Bros., Pokémon, Angry Birds, and more"; //what will the description say?
-CS50G.link = "../ics3up-lrnX-final/courseslist/CS50G.html"; //what should it be linked to? (relative to results.html)
+CS50G.link = "courseslist/CS50G.html"; //what should it be linked to? (relative to results.html)
 CS50G.keywords = ["C++","harvard","cs50","introduction","beginning","lua","games","game development"]; //some keywords that will also bring up the course
 pages.push(CS50G);
 
@@ -264,7 +264,7 @@ W3CIWA.name = "W3CIWA"; //what will the search query be?
 W3CIWA.image = "ics3up-W3CIWA.webp"; //link to image for the course (relative to results.html)
 W3CIWA.display = "W3C's Introduction to Web Authentication"; //what will the bolded heading say?
 W3CIWA.desc =  "Web developers dig deep into modern Web Authentication to secure access to resources and applications"; //what will the description say?
-W3CIWA.link = "../ics3up-lrnX-final/courseslist/W3CIWA.html"; //what should it be linked to? (relative to results.html)
+W3CIWA.link = "courseslist/W3CIWA.html"; //what should it be linked to? (relative to results.html)
 W3CIWA.keywords = ["css","w3c","introduction","beginning","html","web","web development","authentication"]; //some keywords that will also bring up the course
 pages.push(W3CIWA);
 
@@ -273,7 +273,7 @@ ACCAML.name = "ACCAML"; //what will the search query be?
 ACCAML.image = "ics3up-ACCAML.webp"; //link to image for the course (relative to results.html)
 ACCAML.display = "ACCA: Machine Learning with Python for Financial Professionals"; //what will the bolded heading say?
 ACCAML.desc =  "A machine learning course focused on delivering practical Python skills for finance professionals looking to maximise their use of these time-saving tools within their organisation."; //what will the description say?
-ACCAML.link = "../ics3up-lrnX-final/courseslist/ACCAML.html"; //what should it be linked to? (relative to results.html)
+ACCAML.link = "courseslist/ACCAML.html"; //what should it be linked to? (relative to results.html)
 ACCAML.keywords = ["acca","python","ai","machine","financial","finance","machine learning"]; //some keywords that will also bring up the course
 pages.push(ACCAML);
 
@@ -282,8 +282,16 @@ HARVP.name = "HARVP"; //what will the search query be?
 HARVP.image = "ics3up-harvp.jpg"; //link to image for the course (relative to results.html)
 HARVP.display = "Harvard: Using Python for Research"; //what will the bolded heading say?
 HARVP.desc =  "Take your introductory knowledge of Python programming to the next level and learn how to use Python 3 for your research."; //what will the description say?
-HARVP.link = "../ics3up-lrnX-final/courseslist/HARVP.html"; //what should it be linked to? (relative to results.html)
+HARVP.link = "courseslist/HARVP.html"; //what should it be linked to? (relative to results.html)
 HARVP.keywords = ["harvard","python","research","intermediate","student","education"]; //some keywords that will also bring up the course
 pages.push(HARVP);
 
-// TO ADD W3Cjs GAIjs EPFLNJS LNjs NYUIC++
+const W3Cjs = {};
+W3Cjs.name = "W3Cjs"; //what will the search query be?
+W3Cjs.image = "ics3up-W3Cjs.webp"; //link to image for the course (relative to results.html)
+W3Cjs.display = "W3C: Javascript Introduction"; //what will the bolded heading say?
+W3Cjs.desc =  "Learn JavaScript, a Web development programming language, to add interactivity to your Web pages, and become an expert Web developer."; //what will the description say?
+W3Cjs.link = "courseslist/W3Cjs.html"; //what should it be linked to? (relative to results.html)
+W3Cjs.keywords = ["js","javascript","beginner","intro","introduction","student","w3c"]; //some keywords that will also bring up the course
+pages.push(W3Cjs);
+// TO ADD  GAIjs EPFLNJS LNjs NYUIC++ IBMOOPC++
